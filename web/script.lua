8f4082725326f1e5fe2d4cdc83b0873769acdbd0
c00loko <97066553+c00loko@users.noreply.github.com>
2022-01-04 13:54:57 +0100

    Update script.lua

--- web/script.lua
+++ web/script.lua
@@ -1,8 +1,8 @@
-----------website: lever123.000webhostapp.com
+----------website: c00loko.github.io/lever123/web
 -------Exploit Supported:
 ----krnl
 ----synapse x
----made with ❤️ cngy
+---made with ❤️ cngy & c00loko
 
-if game.PlaceId == 155615604 then loadstring(game:HttpGet("https://lever123.000webhostapp.com/g/prisonlife.lua"))(); end;
+if game.PlaceId == 155615604 then loadstring(game:HttpGet("https://c00loko.github.io/lever123/web/g/prisonlife.lua"))(); end;
 if not syn_decompile then if not KRNL_LOADED then game.Players.LocalPlayer:Kick("Use krnl or Synapse X.");end;end;
